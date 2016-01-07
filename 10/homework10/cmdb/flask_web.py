a20cc38f28025dfc8b621df12d839f75e80f6c91
seerjk <seerjk@gmail.com>
2016-01-08 00:51:48 +0800

    homework10 done with modify server settings

--- 10/homework10/cmdb/flask_web.py
+++ 10/homework10/cmdb/flask_web.py
@@ -3,15 +3,17 @@ from flask import Flask, render_template, request
 import MySQLdb as mysql
 import json
 
-con = mysql.connect(host='127.0.0.1', user='root', passwd='redhat', db='jiangkun')
+con = mysql.connect(host='127.0.0.1', user='root',
+                    passwd='redhat', db='jiangkun')
 con.autocommit(True)
 cur = con.cursor()
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    print request.method
+    # print request.method
     if request.method == 'GET':
         return render_template('index.html')
     elif request.method == 'POST':
@@ -23,12 +25,28 @@ def index():
         return json.dumps(res)
 
 
+@app.route('/searchbyid')
+def search_host():
+    host_id = request.args.get('id')
+
+    sql = 'select * from server10 where id=%s' % (host_id)
+
+    try:
+        cur.execute(sql)
+        res = cur.fetchall()
+    except:
+        res = 'error'
+
+    return json.dumps(res)
+
+
 @app.route('/addhost')
-def addhost():
+def add_host():
     name = request.args.get('name')
     mem = request.args.get('mem')
     end_date = request.args.get('end_date')
-    sql = 'insert into server10 (name, memory, end_date) values ("%s", %s, "%s")' % (name, mem, end_date)
+    sql = 'insert into server10 (name, memory, end_date) values ("%s", %s, "%s")' % (
+        name, mem, end_date)
 
     res = 'ok'
     # print sql
@@ -42,8 +60,9 @@ def addhost():
 
     return res
 
+
 @app.route('/delhost')
-def delhost():
+def del_host():
     del_id = request.args.get('id')
     sql = 'delete from server10 where id=%s' % (del_id)
     res = 'ok'
@@ -51,8 +70,29 @@ def delhost():
         cur.execute(sql)
     except:
         res = 'error in sql'
-        
+
     return res
+
+
+@app.route('/modhost')
+def mod_host():
+    mod_id = request.args.get('id')
+    mod_name = request.args.get('name')
+    mod_mem = request.args.get('mem')
+    mod_end_date = request.args.get('end_date')
+
+    sql = 'update server10 set name="%s", memory=%s, end_date="%s" where id=%s' % (
+        mod_name, mod_mem, mod_end_date, mod_id)
+    # print sql
+    res = 'ok'
+    try:
+        cur.execute(sql)
+    except:
+        res = 'error in sql'
+
+    # print res
+    return res
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True, port=9092)
